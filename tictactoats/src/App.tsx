aa7f82e28d8d7a73d60bb3c8dc07fb316caaf9fe
SamuelLaguna <samanater11@gmail.com>
2024-06-27 21:27:05 -0700

    first commit

---
+++ tictactoats/src/App.tsx
@@ -0,0 +1,18 @@
+import Board from "./components/Board"
+import Game from "./components/Game"
+
+
+const App = () => {
+  return (
+    <>
+    <div className="game-container">
+
+    <h1>
+      <Game/>
+      </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
